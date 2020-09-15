02db1c91a0d4bc75997bd563fd797df4fa8402f6
chenpeng <1439575437@qq.com>
2020-09-15 16:29:01 +0800

    修改HelloProject

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
 
+/**
+ * @author Administrator
+ */
 public class Main {
 
     @Test
-    public void testHelloWrold() {
+    public void testHelloWold() {
         System.out.println("HelloWorld!");
     }
+    String s="new";
 }
